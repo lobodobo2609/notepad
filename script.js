efc8112a2fc76f529e7631a59115f5cb73a49a24
kushagra <sharm629@purdue.edu>
2025-06-27 17:51:37 -0400

    Added a download button.

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ notepad.value = localStorage.getItem('notes') || '';
 notepad.addEventListener('input', () => {
   localStorage.setItem('notes', notepad.value);
 });
+
+downloadBtn.addEventListener('click', () => {
+  const blob = new Blob([notepad.value], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'my-notes.txt';
+  link.click();
+  URL.revokeObjectURL(url);
+});
